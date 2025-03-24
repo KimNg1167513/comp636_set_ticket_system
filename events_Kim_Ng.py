9f3a72d69b5fbdf170d60d7ac91b10de68c76bc1
KimNg1167513 <lion.ng@lincolnuni.ac.nz>
2025-03-24 22:44:48 +1300

    Working on buying ticket

--- events_Kim_Ng.py
+++ events_Kim_Ng.py
@@ -3,23 +3,31 @@
 # Student ID : 1167513 
 # ================================================================
  
-from datetime import datetime,timedelta     # datetime module is required for working with dates
+from datetime import datetime,timedelta,date     # datetime module is required for working with dates
 
 # Make the variables and function in set_data.py available in this code (without needing 'set_data.' prefix)
-from set_data import customers,events,unique_id,display_formatted_row   
+from set_data import customers,events,unique_id,display_formatted_row 
+
+# Alternative for testing   
+# from set_data_alternative import customers,events,unique_id,display_formatted_row   
 
 continue_text = "\nPress Enter to continue."
 
 sorted_customers = sorted(customers, key=lambda x: (x[2], x[1])) # ordered by family name, then first name
 sorted_events = {k: events[k] for k in sorted(events)} #  sort the events via dictionary comprehension, shortcut to create a new dictionary.
 
-def list_all_customers():
+def list_all_customers(sorted=False):
     """
     Lists customer details.
     This is an example of how to produce basic output."""
+    if sorted == True:
+        data = sorted_customers
+    else:
+        data = customers
+
     format_str = "{: <5} {: <15} {: <15} {: <14} {: <20}"            # Use the same format_str for column headers and rows to ensure consistent spacing. 
     display_formatted_row(["ID","First Name","Family Name","Birth Date","e-Mail"],format_str)     # Use the display_formatted_row() function to display the column headers with consistent spacing
-    for customer in customers:
+    for customer in data:
         id = customer[0]
         fname = customer[1]
         famname = customer[2]
@@ -29,7 +37,7 @@ def list_all_customers():
         display_formatted_row([id,fname,famname,birthdate,email],format_str)     # Use the display_formatted_row() function to display each row with consistent spacing
     input(continue_text)
 
-def list_customers_and_tickets():
+def list_customers_and_tickets(do_not_continue=False):
     """
     Lists Customer details (including birth date), and the events they have purchased tickets to attend."""
     
@@ -59,28 +67,98 @@ def list_customers_and_tickets():
 
         display_formatted_row([id,famname,fname,birthdate,email,event],format_str)   
 
-    input(continue_text)
+    if do_not_continue == False:
+        input(continue_text)
 
-def list_event_details():
+def list_event_details(age=None):
     """
     List the events, show all details except Customers who have purchased tickets."""
-    format_str = "{: <20} {: <18} {: <15} {: <14} {: <28}"    
-    display_formatted_row(["Event Name","Age Restriction","Event Date","Capacity","Tickets Sold"],format_str)  
-    for event_name in sorted_events:
+    # age is an argument to filter the events for age_restriction, if this argument is provided, the event auto filtered by the date as well
+
+    format_str = "{: <25} {: <18} {: <15} {: <14} {: <28}"    
+    format_str_with_id = "{: <10} {: <18} {: <15} {: <14} {: <28}"    
+    
+    if age:
+        display_formatted_row(["ID","Event Name","Event Date","Capacity","Tickets Sold"],format_str_with_id)  
+        result=[]
+    else:
+        display_formatted_row(["Event Name","Age Restriction","Event Date","Capacity","Tickets Sold"],format_str)  
+
+    for index, event_name in enumerate(sorted_events, start=1):
         event = sorted_events[event_name]
+        if age:
+            # Check if the event has passed, the age restriction and if the tickets are sold up
+            if datetime.now().date() >= event['event_date'] or age < int(event['age_restriction']) or event["capacity"] - event['tickets_sold'] <= 0:
+                continue
+
+        event_id = index
         name=event_name
         age_restriction=str(event['age_restriction'])
         date=event['event_date'].strftime("%d %b %Y")
         capacity=str(event['capacity'])
         tickets_sold=str(event['tickets_sold'])
-        display_formatted_row([name,age_restriction,date,capacity,tickets_sold],format_str) \
-        
-    input(continue_text)
+        if age:
+            display_formatted_row([event_id,name,date,capacity,tickets_sold],format_str_with_id)
+            result.append({"id":event_id,"name":name ,"age_restriction":age_restriction,"event_date":date,"capacity":capacity,"tickets_sold":tickets_sold, "customers":event['customers']})
+        else:
+            display_formatted_row([name,age_restriction,date,capacity,tickets_sold],format_str)
+            input(continue_text)
+    
+    if age:
+        return result
 
 def buy_tickets():
     """
     Choose a customer, then a future event, the purchase can only proceed if they meet the minimum age requirement and tickets are available """
-    pass  # REMOVE this line once you have some function code (a function must have one line of code, so this temporary line keeps Python happy so you can run the code)
+    
+    # Display ordered customer name and ask which customer you want to buy for
+    def getCustomerID():
+        list_customers_and_tickets(True)
+        print("")
+        customer_id = input('Please select a customer by entering ID or enter "X" to escape: ')
+        # return customer_id
+        
+        if customer_id:
+            if customer_id == 'X' or customer_id == 'x':
+                pass
+            else:
+                try:
+                    customer_id = int(customer_id)
+                    print(f'Checking the given ID:({customer_id})...')
+                    target_customer = next((customer for customer in sorted_customers if customer[0] == customer_id), None)
+                    if target_customer == None:
+                        print(f'ID: {customer_id} not found, please enter an valid ID.')
+                        print('')
+                        getCustomerID()
+                    else:
+                        # Found the customer, display events
+                        # Check the DOB and today's date
+                        age = datetime.now().year - target_customer[3].year
+                        available_events = list_event_details(age)
+                        print(available_events)
+                        # target_event_id = int(input('Please enter the Event ID: '))
+                        # while True:
+                        #     try:
+                        #         target_event_id = int(input('Please enter the Event ID: '))
+                        #         break
+                        #     except ValueError:
+                        #         print('Please enter an valid Event ID.')
+
+                        # print(sorted_events)
+                except ValueError:
+                    print("Please enter an valid ID.")
+                    print('')
+                    getCustomerID()            
+        else:
+            print("!!! Please select a customer to continue.")
+            getCustomerID()
+
+        
+    # Display filter events (age restriction and date) and ask which event
+    # Multiple events?
+
+    getCustomerID()
+    # input(continue_text) 
 
 def add_new_customer():
     """
@@ -111,6 +189,7 @@ def disp_menu():
 print("")
 print("### Disable the main program for now so I can do some dev ###")
 print("")
+buy_tickets()
 
 
 # ------------ This is the main program ------------------------
@@ -119,35 +198,35 @@ print("")
 # Although you can add arguments to the function calls, if you wish.
 # Repeat this loop until the user enters an "X" or "x"
 
-response = ""
-while response != "X":
-    disp_menu()
+# response = ""
+# while response != "X":
+#     disp_menu()
     
-    print("")
-    # Display menu for the first time, and ask for response
-    response = input("Please enter menu choice: ")    
-    print("")
+#     print("")
+#     # Display menu for the first time, and ask for response
+#     response = input("Please enter menu choice: ")    
+#     print("")
 
-    # Make the string uppercase either x or X is able to exit the program
-    if response == "x":
-        response = response.upper()
+#     # Make the string uppercase either x or X is able to exit the program
+#     if response == "x":
+#         response = response.upper()
 
-    if response == "1":
-        list_all_customers()
-    elif response == "2":
-        list_customers_and_tickets()
-    elif response == "3":
-        list_event_details()
-    elif response == "4":
-        buy_tickets()
-    elif response == "5":
-        list_future_available_events()
-    elif response == "6":
-        add_new_customer()
-    elif response != "X":
-        print("\n*** Invalid response, please try again (enter 1-6 or X)")
+#     if response == "1":
+#         list_all_customers()
+#     elif response == "2":
+#         list_customers_and_tickets()
+#     elif response == "3":
+#         list_event_details()
+#     elif response == "4":
+#         buy_tickets()
+#     elif response == "5":
+#         list_future_available_events()
+#     elif response == "6":
+#         add_new_customer()
+#     elif response != "X":
+#         print("\n*** Invalid response, please try again (enter 1-6 or X)")
 
-    print("")
+#     print("")
 
-print("\n=== Thank you for using the SELWYN EVENT TICKET SYSTEM! ===\n")
+# print("\n=== Thank you for using the SELWYN EVENT TICKET SYSTEM! ===\n")
 
